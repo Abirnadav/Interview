0bf51259d255c2698a6a07aace2c83ca5fec89cf
Abirnadav <62353063+Abirnadav@users.noreply.github.com>
2020-06-03 11:51:23 +0300

    Right path

---
+++ precache-manifest.57ed32fdfc2885cf25b60991a49c7892.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc91a32f87f118aadca275a1a974cd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8310851709758c1f598f",
+    "url": "/static/css/main.4b3278dd.chunk.css"
+  },
+  {
+    "revision": "58fb5763fc4e2fefb75f",
+    "url": "/static/js/2.eace7c68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eace7c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8310851709758c1f598f",
+    "url": "/static/js/main.4c740704.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "d1d950e09ee94a2bbdd4627e65143cbc",
+    "url": "/static/media/avatar-aiysha.d1d950e0.jpg"
+  },
+  {
+    "revision": "60ff8909e4d598e25c144f0b47d988e3",
+    "url": "/static/media/avatar-arthur.60ff8909.jpg"
+  },
+  {
+    "revision": "f16826cacf2b1a460d2eab969d343d0a",
+    "url": "/static/media/avatar-kady.f16826ca.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
